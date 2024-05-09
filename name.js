c12155085be359fb597fb3751b7541ce406296bb
yadavshravani <147603776+yadavshravani@users.noreply.github.com>
2024-05-10 00:16:54 +0530

    Create name.js

---
+++ name.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form submission handling
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+   
+});
